0d864ce75a8cefd78fb7b2d59c5551c2c0b1aee2
Picobola1 <bonilla.ellie@gmail.com>
2026-08-04 21:52:13 -0700

    i have a grid that l ooks like an actual gird

--- main.py
+++ main.py
@@ -13,13 +13,20 @@ def draw_block_grid(screen, row, col, tile_size):
              y = r * tile_size
 
              Rect = pygame.Rect(x,y,tile_size,tile_size)
-             pygame.draw.rect(screen,(0,0,255),Rect, 1)
+             
+             pygame.draw.rect(screen,(128, 128, 128),Rect, 0)
+             pygame.draw.line(screen,"black",(0,r * tile_size),(750,r * tile_size))
+             pygame.draw.line(screen,"black",(c * tile_size,0),(c * tile_size,750))
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+
     #pygame.draw.rect(screen, (0,0,255), (0,0,30,30))
     draw_block_grid(screen, 150, 150, tile_size)
+    pygame.draw.rect(screen,(0, 0, 255),(0,0,30,30), 0)
     pygame.display.flip()
+    
